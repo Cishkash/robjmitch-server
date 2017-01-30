3d354d9516bd2a3b29abde130ce3c430dbb01f4f
Robby Mitchell <robjmitch@gmail.com>
2017-01-29 22:57:38 -0800

    Adjusted and pruned unused routes and set up firebase

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
+var firebase = require('firebase');
 var express = require('express');
 var router = express.Router();
+var config = require('../.env');
 
-/* GET home page. */
+firebase.initializeApp(config);
+
+// Testing firebase integration
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  function writeUserData(userId, name, email, imageUrl) {
+    // Firebase doesn't automatically send a response with set so we create one
+    return new Promise( (resolve, reject) => {
+      let postUser = firebase.database().ref('users/' + userId).set({
+        name: name,
+        email: email,
+        image: imageUrl
+      });
+      if (postUser) {
+        resolve();
+      } else {
+        reject();
+      }
+    });
+  }
+
+  // These are just test parameters
+  writeUserData(120, 'Robby', 'robjmitch@derp.com', 'kitty').then(
+    // Resolved `.set()`
+    () => {
+      res.send('Posted!');
+    // Rejected `.set()`
+    }, (ex) => {
+      res.send('postUser failed:', ex);
+    }
+  );
 });
 
 module.exports = router;
