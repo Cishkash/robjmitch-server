1a245426f490d8555fa28b65928e149cefff9192
Robby Mitchell <robjmitch@gmail.com>
2017-02-03 23:49:45 -0800

    Adds CORS access control to localhost

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,12 @@ const express = require('express');
 const router = express.Router();
 const config = require('../.env');
 
+router.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 firebase.initializeApp(config);
 
 // Testing firebase integration
