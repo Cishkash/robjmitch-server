7eca2439c63102775e6b09a892c01e8ee55420f5
Robby Mitchell <robjmitch@gmail.com>
2017-03-13 22:21:40 -0700

    Removed a host from CORS

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const config = require('../.env');
 
 router.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000", "http://robjmitch.com");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
