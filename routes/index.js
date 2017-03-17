a78cfc3612e44e27c5997ea1f4d39967e3f08424
Robby Mitchell <robjmitch@gmail.com>
2017-03-16 21:00:32 -0700

    Opted for .includes since node version is high enough

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.use(function(req, res, next) {
   const allowedOrigins = ['http://localhost:3000', 'http://robjmitch.com'];
   const origin = req.headers.origin;
 
-  if (!allowedOrigins.indexOf(origin)) {
+  if (allowedOrigins.includes(origin)) {
     res.header("Access-Control-Allow-Origin", origin);
   }
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
