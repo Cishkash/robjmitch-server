83620e0f25eac7d550ce1006b202e702483c1e84
Robby Mitchell <robjmitch@gmail.com>
2017-03-16 20:35:41 -0700

    Sets up CORS for multiple domains

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,15 @@ const router = express.Router();
 const config = require('../.env');
 
 router.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  // Setup CORS for multiple domains
+  const allowedOrigins = ['http://localhost:3000', 'http://robjmitch.com'];
+  const origin = req.headers.origin;
+
+  if (!allowedOrigins.indexOf(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Credentials', true);
   next();
 });
 
